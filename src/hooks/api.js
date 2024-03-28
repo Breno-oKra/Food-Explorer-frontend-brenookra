f312a4127f7c42d31a7c047415528bfe0d1fbded
breno-okra <brenookra@gmail.com>
2024-03-28 13:43:11 -0300

    change axios

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-/* export const api = axios.create({
-  baseURL: 'https://foodexplorer-api-brenookra.onrender.com',
-}); */
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://foodexplorer-api-brenookra.onrender.com',
 });
